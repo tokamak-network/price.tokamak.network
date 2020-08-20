4a24edc7bfa9f578398a4c4b45f36f2ae620c6b2
Lakmi <lakmi94@gmail.com>
2020-08-20 13:50:07 +0900

    update deploy files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+const isDev = process.env.NODE_ENV !== 'production';
+Vue.config.performance = isDev;
 
 new Vue({
   render: h => h(App),
