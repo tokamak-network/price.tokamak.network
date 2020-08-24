11172081cfa53517aae213207ace2fd035f41317
Lakmi <lakmi94@gmail.com>
2020-08-24 13:43:38 +0900

    dropdown menu added

--- src/containers/HeaderContainer.vue
+++ src/containers/HeaderContainer.vue
@@ -7,6 +7,25 @@
           Tokamak Network Price Dashboard
         </div>
       </div>
+      <div class="dropdown">
+        <button class="dropbtn" @click="showMenu">
+          <img src="@/assets/images/hamburgerIcon.png">
+        </button>
+        <div id="myDropdown" class="dropdown-content">
+          <a
+            class="header-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://tokamak.network"
+          >What is TON?</a>
+          <a
+            class="header-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://upbit.com/exchange?code=CRIX.UPBIT.BTC-TON"
+          >Buy TON</a>
+        </div>
+      </div>
       <div class="header-link-container">
         <a
           class="header-link"
@@ -28,7 +47,14 @@
 
 <script>
 
-export default {};
+export default {
+  name: 'HeaderContainer',
+  methods: {
+    showMenu () {
+      document.getElementById('myDropdown').classList.toggle('show');
+    },
+  },
+};
 </script>
 
 <style>
@@ -92,14 +118,55 @@ html, body {
   text-align: left;
   color: #7e8d93;
 }
+.dropdown {
+  display: none;
+  flex-direction: column;
+}
 @media screen and (max-width: 650px) {
   .header {
   width: 960px;
   height: 100%;
   align-items: center;
   display: flex;
-  padding-left: 5px;
+  padding: 0px 10px;
+}
+.dropdown {
+ position: relative;
+  display: inline-block;
+}
+button:hover {
+  cursor: pointer;
+}
+button:focus {
+  outline: none;
+}
+.dropbtn {
+  flex-direction: row;
+  display: flex;
+  justify-content: end;
+  background: #ffffff;
+  border: none;
+  padding-left: 10px;
+}
+.dropdown-content {
+   display: none;
+   position: absolute;
+   overflow: auto;
+   right: 0px;
+    z-index: 1;
+}
+
+.header-link-container{
+  display: none;
+}
+.show {
+  display: flex;
   flex-direction: column;
+  background: #ffffff;
+  width: 125px;
+}
+.header-link{
+padding: 10px;
 }
 }
 </style>
