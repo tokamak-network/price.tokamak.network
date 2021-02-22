42d85355778b50d6b41edf1cd79769cfa8d427a0
Lakmi <lakmi94@gmail.com>
2021-02-22 10:48:05 +0900

    tab price fixed

--- src/containers/Dashboard.vue
+++ src/containers/Dashboard.vue
@@ -1,21 +1,95 @@
 <template>
-  <div class="dashbboard-container">
-    <div class="date">{{ currentTime }}</div>
-    <div class="row">
-      <TextViewerTon :title="'TON Price'" :KRWValue="info.trade_price" :USDValue="info.trade_price*usd" />
-      <TextViewer :title="'Market Cap'" :krw="info.trade_price*totalSupply" :usd="totalSupply*info.trade_price*usd" :ton="totalSupply" :subTitle="'Total Supply'" :tooltip="''" />
-      <TextViewerTon :title="'Trading Volume'" :KRWValue="info.acc_trade_price_24h" :USDValue="info.acc_trade_price_24h*usd" />
+  <div>
+    <div v-if="loaded" class="dashbboard-container">
+      <div class="date">{{ currentTime }}</div>
+      <div class="row">
+        <TextViewerTon
+          :title="'TON Price'"
+          :KRWValue="info.trade_price"
+          :USDValue="info.trade_price * usd"
+        />
+        <TextViewer
+          :title="'Market Cap'"
+          :krw="info.trade_price * totalSupply"
+          :usd="totalSupply * info.trade_price * usd"
+          :ton="totalSupply"
+          :subTitle="'Total Supply'"
+          :tooltip="''"
+        />
+        <TextViewerTon
+          :title="'Trading Volume'"
+          :KRWValue="info.acc_trade_price_24h"
+          :USDValue="info.acc_trade_price_24h * usd"
+        />
+      </div>
+      <div class="row">
+        <TextViewer
+          :title="'Market Cap'"
+          :krw="circulatingSupply * info.trade_price"
+          :usd="circulatingSupply * info.trade_price * usd"
+          :ton="circulatingSupply"
+          :subTitle="'Circulating Supply'"
+          :tooltip="'true'"
+        />
+        <TextViewerStaked
+          :title="'Total Staked TON'"
+          :KRWValue="
+            totalStaked.toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+              minimumFractionDigits: 2,
+            })
+          "
+          :USDValue="
+            ((totalStaked / circulatingSupply) * 100).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+              minimumFractionDigits: 2,
+            })
+          "
+        />
+        <TextViewerStaked
+          :title="'Total Tradable TON'"
+          :KRWValue="
+            (circulatingSupply - totalStaked).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+              minimumFractionDigits: 2,
+            })
+          "
+          :USDValue="
+            (
+              ((circulatingSupply - totalStaked) / circulatingSupply) *
+              100
+            ).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+              minimumFractionDigits: 2,
+            })
+          "
+        />
+      </div>
+      <div class="row">
+        <TextViewerBottom
+          :title="'Opening Price'"
+          :krw="info.opening_price"
+          :usd="info.opening_price * usd"
+        />
+        <TextViewerBottom
+          :title="'Closing Price'"
+          :krw="info.prev_closing_price"
+          :usd="info.prev_closing_price * usd"
+        />
+        <TextViewerBottom
+          :title="'High Price'"
+          :krw="info.high_price"
+          :usd="info.high_price * usd"
+        />
+        <TextViewerBottom
+          :title="'Low Price'"
+          :krw="info.low_price"
+          :usd="info.low_price * usd"
+        />
+      </div>
     </div>
-    <div class="row">
-      <TextViewer :title="'Market Cap'" :krw="circulatingSupply*info.trade_price" :usd="circulatingSupply*info.trade_price*usd" :ton="circulatingSupply" :subTitle="'Circulating Supply'" :tooltip="'true'" />
-      <TextViewerStaked :title="'Total Staked TON'" :KRWValue="totalStaked.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })" :USDValue="((totalStaked/circulatingSupply)*100).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })" />
-      <TextViewerStaked :title="'Total Tradable TON'" :KRWValue="(circulatingSupply-totalStaked).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })" :USDValue="(((circulatingSupply-totalStaked)/circulatingSupply)*100).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })" />
-    </div>
-    <div class="row">
-      <TextViewerBottom :title="'Opening Price'" :krw="info.opening_price" :usd="info.opening_price*usd" />
-      <TextViewerBottom :title="'Closing Price'" :krw="info.prev_closing_price" :usd="info.prev_closing_price*usd" />
-      <TextViewerBottom :title="'High Price'" :krw="info.high_price" :usd="info.high_price*usd" />
-      <TextViewerBottom :title="'Low Price'" :krw="info.low_price" :usd="info.low_price*usd" />
+    <div v-else class="spinner-container">
+      <loading-spinner />
     </div>
   </div>
 </template>
@@ -26,6 +100,8 @@ import TextViewerStaked from '@/components/TextViewerStaked.vue';
 import TextViewerBottom from '@/components/TextViewerBottom.vue';
 import TextViewerTon from '@/components/TextViewerTon.vue';
 import moment from 'moment';
+import { mapState, mapGetters } from 'vuex';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
 
 const axios = require('axios');
 export default {
@@ -35,107 +111,20 @@ export default {
     TextViewerBottom,
     TextViewerTon,
     TextViewerStaked,
+    'loading-spinner': LoadingSpinner,
   },
-  props :{
-  },
-  data () {
-    return {
-      info: {
-        type: Object,
-      },
-      btc: {
-        type: Number,
-      },
-      usd: 0,
-      circulatingSupply: 0,
-      currentTime: null,
-      totalStaked:0,
-      totalSupply:0,
-    };
-  },
-
+  props: {},
   computed: {
-    getPrice () {
-      return this.info.trade_price;
-    },
-  },
-  created () {
-
-    const moments = require('moment-timezone');
-    const zoneName =  moments.tz.guess();
-    const timezone = moments.tz(zoneName).zoneAbbr();
-    this.currentTime = moment().format('DD/MM/YYYY hh:mm:ss ') + timezone;
-    setInterval(() => this.updateCurrentTime(), 1000);
-    this.getCurrencyInfo();
-    setTimeout(() => {this.updateTitle();}, 100);
-    setInterval(() => {this.getCurrencyInfo();}, 300000 );
-    setInterval(() => this.updateTitle(), 60000);
-    this.getUSDInfo();
-    setInterval(() => {this.getUSDInfo();}, 30000 );
-    this.getCirculatingSupply();
-    setInterval(() => {this.getCirculatingSupply();}, 1800000 );
-    setInterval(() => {this.getTotalStaked();}, 30000 );
-    this.getTotalStaked();
-    setInterval(() => {this.getTotalSupply();}, 30000 );
-    this.getTotalSupply();
-
-
-  },
-  methods: {
-    updateCurrentTime () {
-      const moments = require('moment-timezone');
-      const zoneName =  moments.tz.guess();
-      const timezone = moments.tz(zoneName).zoneAbbr();
-      this.currentTime = moment().format('DD/MM/YYYY hh:mm:ss ') + timezone;
-    },
-    getCurrencyInfo () {
-      axios
-        .get('https://api.upbit.com/v1/ticker?markets=KRW-TON')
-        .then(response => (
-          this.info = JSON.parse(JSON.stringify(response.data).replace(/]|[[]/g, ''))
-
-        ));
-
-    },
-    getUSDInfo () {
-      axios
-        .get('https://api.frankfurter.app/latest?from=KRW')
-        .then(response => (
-          this.usd = response.data.rates.USD
-        ));
-    },
-    getCirculatingSupply () {
-      axios
-        .get('https://price-api.tokamak.network/circulatedcoins ')
-        .then (response => {
-          this.circulatingSupply = response.data;
-        });
-    },
-    getTotalStaked () {
-      axios
-        .get('https://price-api.tokamak.network/staking/current')
-        .then (response => {
-          this.totalStaked = response.data;
-        });
-    },
-    getTotalSupply () {
-      axios
-        .get('https://price-api.tokamak.network/totalsupply')
-        .then (response => {
-          this.totalSupply = response.data;
-        });
-    },
-    updateTitle () {
-      if (this.info.trade_price === undefined) {
-        const title = 'Tokamak Network Price Dashboard';
-        document.title = title;
-      }
-      else {
-        const title = Math.trunc(this.info.trade_price).toLocaleString('en-US') + ' TON/KRW';
-        document.title = title;
-      }
-
-    },
+    ...mapState([
+      'info',
+      'btc',
+      'circulatingSupply',
+      'usd',
+      'currentTime',
+      'totalStaked',
+      'totalSupply',
+      'loaded',
+    ]),
   },
 };
 </script>
@@ -156,27 +145,34 @@ export default {
 .row {
   display: flex;
   flex-direction: row;
-  justify-content:center;
+  justify-content: center;
 }
 
+.spinner-container {
+  height: 100%;
+  display: flex;
+    justify-content: center;
+    align-items: center;
+}
 @media screen and (max-width: 600px) {
   .dashbboard-container {
-  margin-top: 10px;
-  display: flex;
-  flex-flow: column wrap;
-  flex-direction: column;
-  position: relative;
-}
-.date {
-  font-weight: bold;
-  font-size: 40px;
-  padding-bottom: 20px;
-}
-.row {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-self: center;
-}
+    margin-top: 10px;
+    display: flex;
+    flex-flow: column wrap;
+    flex-direction: column;
+    position: relative;
+  }
+  .date {
+    font-weight: bold;
+    font-size: 40px;
+    padding-bottom: 20px;
+  }
+  .row {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-self: center;
+  }
+
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,131 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+const moments = require('moment-timezone');
+const axios = require('axios');
+import moment from 'moment';
+
+export default new Vuex.Store({
+  state: {
+    info: {},
+    loaded: false,
+    btc: {},
+    usd: 0,
+    circulatingSupply: 0,
+    currentTime: null,
+    totalStaked: 0,
+    totalSupply: 0,
+    title: 'Tokamak Network Price Dashboard',
+  },
+  mutations: {
+    SET_CURRENT_TIME: (state, time) => {
+      state.currentTime = time;
+    },
+    SET_INFO: (state, info) => {
+      state.info = info;
+    },
+    SET_TITLE: (state, title) => {
+      state.title = title;
+    },
+    SET_BTC: (state, btc) => {
+      state.btc = btc;
+    },
+    SET_LOADED: (state, loaded) => {
+      state.loaded = loaded;
+    },
+    SET_CIRCULATINGSUPPLY: (state, circulatingSupply) => {
+      state.circulatingSupply = circulatingSupply;
+    },
+    SET_TOTALSTAKED: (state, totalStaked) => {
+      state.totalStaked = totalStaked;
+    },
+    SET_TOTALSUPPLY: (state, totalSupply) => {
+      state.totalSupply = totalSupply;
+    },
+    SET_USD: (state, usd) => {
+      state.usd = usd;
+    },
+  },
+  actions: {
+    // setData (context) {
+    //   context.dispatch('updateCurrentTime');
+    // },
+    setCurrentTime (context) {
+      const moments = require('moment-timezone');
+      const zoneName = moments.tz.guess();
+      const timezone = moments.tz(zoneName).zoneAbbr();
+      const currentTime = moment().format('DD/MM/YYYY hh:mm:ss ') + timezone;
+      context.commit('SET_CURRENT_TIME', currentTime);
+    },
+    async setData (context) {
+      await Promise.all([
+        context.dispatch('setCurrentTime'),
+        context.dispatch('getCurrencyInfo'),
+        context.dispatch('getUSDInfo'),
+        context.dispatch('getCirculatingSupply'),
+        context.dispatch('getTotalStaked'),
+        context.dispatch('getTotalSupply'),
+      ]);
+
+      context.dispatch('setLoaded');
+      const info = await context.state.info.trade_price;
+      const title = Math.trunc(info).toLocaleString('en-US') + ' TON/KRW';
+      document.title = title;
+    //   if (context.state.info !== {}) {
+    //     context.commit('SET_LOADED', true);
+    //   }
+    },
+    updateData (context) {
+      setInterval(() => context.dispatch('setCurrentTime'), 1000);
+      setInterval(() => context.dispatch('getCurrencyInfo'), 60000);
+      setInterval(() => context.dispatch('getUSDInfo'), 60000);
+      setInterval(() => context.dispatch('getCirculatingSupply'), 60000);
+      setInterval(() => context.dispatch('getTotalStaked'), 60000);
+      setInterval(() => context.dispatch('getTotalSupply'), 60000);
+    },
+    async getCurrencyInfo (context) {
+      await axios
+        .get('https://api.upbit.com/v1/ticker?markets=KRW-TON')
+        .then(response => {
+
+          context.commit(
+            'SET_INFO',
+            JSON.parse(JSON.stringify(response.data).replace(/]|[[]/g, '')),
+          );
+          if(context.state.loaded){
+            document.title  =  Math.trunc(response.data[0].trade_price).toLocaleString('en-US') + ' TON/KRW';
+
+          }
+        }   ); },
+    async getUSDInfo (context) {
+      await axios
+        .get('https://api.frankfurter.app/latest?from=KRW')
+        .then((response) => context.commit('SET_USD', response.data.rates.USD));
+    },
+    async getCirculatingSupply (context) {
+      await axios
+        .get('https://price-api.tokamak.network/circulatedcoins ')
+        .then((response) => {
+          context.commit('SET_CIRCULATINGSUPPLY', response.data);
+        });
+    },
+    async getTotalStaked (context) {
+      await axios
+        .get('https://price-api.tokamak.network/staking/current')
+        .then((response) => {
+          context.commit('SET_TOTALSTAKED', response.data);
+        });
+    },
+    async getTotalSupply (context) {
+      await axios
+        .get('https://price-api.tokamak.network/totalsupply')
+        .then((response) => {
+          context.commit('SET_TOTALSUPPLY', response.data);
+        });
+    },
+    async setLoaded (context) {
+      await context.commit('SET_LOADED', true);
+    },
+  },
+  getters: {},
+});
